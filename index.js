6700c149c0b8c990ab04d340533c21ef2e13088f
sana chowdhury:git config --global user.name sana <sana.chowdhury86@gmail.com>
2021-06-23 18:11:27 -0500

    added prompts

--- index.js
+++ index.js
@@ -1,7 +1,42 @@
 // TODO: Include packages needed for this application
+const fs = require("fs");
+const fs = require("path");
+const inquirer = require("inquirer");
+const generate = require("./utils/generateMarkdown");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  {
+    type: "input",
+    name: "title",
+    message: "enter project title",
+  },
+  {
+    type: "input",
+    name: "description",
+    message: "enter project description",
+  },
+  {
+    type: "input",
+    name: "installation instructions",
+    message: "enter installation instruction",
+  },
+  {
+    type: "input",
+    name: "usage information",
+    message: "enter usage information",
+  },
+  {
+    type: "input",
+    name: "contribution guidelines",
+    message: "enter contribution guidelines",
+  },
+  {
+    type: "input",
+    name: "test instructions",
+    message: "enter test instructions",
+  },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
